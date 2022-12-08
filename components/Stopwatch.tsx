17609e20b8dd4ef1674a84ffac0cb837a8cf9633
Louisa Goncharenko <louisa.goncharenko@shopify.com>
2022-12-08 14:58:50 -0500

    created stopwatch componenent

---
+++ components/Stopwatch.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native';
+
+const Stopwatch = () => {
+  return (
+    <View></View>
+  )
+}
+
+const styles = StyleSheet.create(
+    {
+      
+
+    })
+
+  
+
+export default Stopwatch
