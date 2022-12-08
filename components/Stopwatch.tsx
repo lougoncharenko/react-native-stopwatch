fdd5c85a1b186ce2f8692329f4a929e2ca7cd14a
Louisa Goncharenko <louisa.goncharenko@shopify.com>
2022-12-08 15:02:54 -0500

    built out components

--- components/Stopwatch.tsx
+++ components/Stopwatch.tsx
@@ -1,9 +1,18 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native';
+import { Pressable, StyleSheet, Text, View } from 'react-native';
 
 const Stopwatch = () => {
   return (
-    <View></View>
+    <View style={styles.container}>
+        <View style={styles.timerDisplay}>
+            <View style={styles.buttons}> 
+                <Pressable style={styles.startButton}></Pressable>
+                <Pressable style={styles.lapButton}></Pressable>
+                <Pressable style={styles.pauseButton}></Pressable>
+                <Pressable style={styles.resetButton}></Pressable>
+            </View>
+        </View>
+    </View>
   )
 }
 
