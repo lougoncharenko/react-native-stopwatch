5c3dfa4b28b7f046259866f463d9cb34aa1e6e59
Louisa Goncharenko <louisa.goncharenko@shopify.com>
2022-12-08 15:42:07 -0500

    added functionality on buttons

--- components/Stopwatch.tsx
+++ components/Stopwatch.tsx
@@ -6,22 +6,35 @@ const Stopwatch = () => {
   const [timer, setTimer] = useState('00 : 00 : 000')
   let [milliseconds,seconds,minutes] = [0,0,0,];
   const [int, setInt] = useState<any>()
+  // Button Functions
+  const handleStart = () => {
+    alert('Timer Starter')
+  }
+  const handleLap = () => {
+    alert('Time Lapped')
+  }
+  const handlePause = () => {
+    alert('Timer Paused')
+  }
+  const handleReset = () => {
+    alert('Timer reset')
+  }
 
   return (
     <View style={styles.container}>
         <View style={styles.timerDisplay}>
           <Text style={styles.timer}>{timer}</Text>
             <View style={styles.buttons}> 
-                <Pressable style={styles.startButton}>
+                <Pressable onPress={handleStart} style={styles.startButton}>
                   <Text>Start</Text>
                 </Pressable>
-                <Pressable style={styles.lapButton}>
+                <Pressable onPress={handleLap} style={styles.lapButton}>
                   <Text>Lap</Text>
                 </Pressable>
-                <Pressable style={styles.pauseButton}>
+                <Pressable onPress = {handlePause} style={styles.pauseButton}>
                   <Text>Pause</Text>
                 </Pressable>
-                <Pressable style={styles.resetButton}>
+                <Pressable onPress = {handleReset} style={styles.resetButton}>
                   <Text>Reset</Text>
                 </Pressable>
             </View>
